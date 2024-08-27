b590fb370943832fea77cfcb42e8e9ba24122ad1
Magdalena Cullen <106096956+mcullen212@users.noreply.github.com>
2024-08-26 21:28:30 -0300

    Started implementing slave md5 hash

--- includes/error.h
+++ includes/error.h
@@ -5,7 +5,9 @@
 #include <errno.h>
 
 #define ERROR -1
-#define PIPE_ERROR "Error: creating pipe"
+#define PIPE_CREATING_ERROR "Error: creating pipe"
+#define PIPE_OPENING_ERROR "Error: opening from pipe"
+#define PIPE_CLOSING_ERROR "Error: closing pipe"
 #define FORK_ERROR "Error: creating child process"
 #define EXEC_ERROR "Error: executing command"
 #define WAITPID_ERROR "Error: waiting for child process"
--- slave.c
+++ slave.c
@@ -1,6 +1,6 @@
 #include "includes/slave.h"
 #include "includes/error.h"
-
+//popen() --> pipe open 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         char file_name[256];
@@ -9,21 +9,36 @@ int main(int argc, char *argv[]) {
             fgets(file_name, sizeof(file_name), stdin);
             file_name[strcspn(file_name, "\n")] = 0; // Remove newline from input
 
-            if(strlen(file_name) > 0){
+            if(strlen(file_name) > 0){  // a file name was entered
                 printf("Procesando archivo %s\n", file_name);
-                //createChildProcess(1, file_name);
-            } else{
-
+                break;
             }
-
-
         }
         
     }
 
     for (int i = 1; i < argc; i++) {
-        printf("Procesando archivo %s\n", argv[i]);
+        //printf("Procesando archivo %s\n", argv[i]);
     }
 
     return 0;
+}
+
+void createMD5(char *file_name) {
+    char command[256];
+
+    // create md5sum command to execute in the pipe created
+    snprintf(command, sizeof(command), "md5sum %s", file_name);
+
+    // open pipe to execute md5sum command
+    FILE *md5 = popen(command, "r");
+
+    if(md5 == NULL){
+        check_error(ERROR, PIPE_OPENING_ERROR);
+    }
+
+    // get stdout from md5sum
+    
+
+    check_error(pclose(md5), PIPE_CLOSING_ERROR);
 }
